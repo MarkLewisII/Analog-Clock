9424d38dea3ee13ae4e2906cc60546b96da41c37
MarkLewisII <77941914+MarkLewisII@users.noreply.github.com>
2026-02-18 10:19:00 -0400

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function GetCurrentTime() {
+    //Grab current time
+}
+
+function SplitCurrentTime(currentTime) {
+    //Split time into Hours, Minutes, Seconds
+}
+
+//Convert Hours, Minutes, and Seconds into angle on circle for clock hands rotation
+function HourHandRotation() {
+    
+} 
+
+function MinuteHandRotation() {
+    
+}
+ 
+function SecondHandRotation() {
+    
+} 
