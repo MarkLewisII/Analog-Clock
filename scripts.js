893a72870d0ad7fe182f146e6fe9e9eae8d85bb8
MarkLewisII <77941914+MarkLewisII@users.noreply.github.com>
2026-02-19 20:48:54 -0400

    Added Digital Clock

--- scripts.js
+++ scripts.js
@@ -1,20 +1,36 @@
-function GetCurrentTime() {
+function updateTime() {
     //Grab current time
+    var currentdate = new Date();
+    let seconds = currentdate.getSeconds();
+    let minutes = currentdate.getMinutes();
+    let hours= currentdate.getHours() % 12;
+
+    // let timeText = `${hours}:${minutes}:${seconds}`;
+    let timeText = currentdate.toLocaleTimeString();
+    const digitalclock = document.getElementById('digitalclock');
+
+    if (digitalclock) {
+        digitalclock.textContent = timeText;
+    }
 }
 
-function SplitCurrentTime(currentTime) {
-    //Split time into Hours, Minutes, Seconds
-}
+updateTime();
+
+
+const updateInterval = setInterval(updateTime, 1000);
+// function SplitCurrentTime(currentTime) {
+//     //Split time into Hours, Minutes, Seconds
+// }
 
 //Convert Hours, Minutes, and Seconds into angle on circle for clock hands rotation
-function HourHandRotation() {
+function hourHandRotation() {
     
 } 
 
-function MinuteHandRotation() {
+function minuteHandRotation() {
     
 }
- 
-function SecondHandRotation() {
+
+function secondHandRotation() {
     
 } 
